8d07f8962cfb33f8a28d4fbc0a601698f8c84d72
AmirMohammad <amir.kordlu@gmail.com>
2023-03-08 02:30:31 +0330

    Force LTR

--- app/src/main/java/com/amk/market/ui/MainActivity.kt
+++ app/src/main/java/com/amk/market/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.amk.market.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -36,6 +37,7 @@ import org.koin.android.ext.koin.androidContext
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
         setContent {
 
             Koin(appDeclaration = {
@@ -47,7 +49,7 @@ class MainActivity : ComponentActivity() {
                         color = BackgroundMain,
                         modifier = Modifier.fillMaxSize()
                     ) {
-                        val userRepository :UserRepository = get()
+                        val userRepository: UserRepository = get()
                         userRepository.loadToken()
                         DuniBazaarUI()
                     }
